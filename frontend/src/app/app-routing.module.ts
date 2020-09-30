a6b882c9e7a1a12846cabb7bcac67783f251ffbd
Győri Gergő <gyori.gergo@encosoft-dev.hu>
2020-10-01 01:55:37 +0200

    Renamed auth page component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthPageComponent } from './core/pages/auth.page/auth.page.component';
+import { AuthPageComponent } from './core/pages/auth/auth.page.component';
+import { StrategyPageComponent } from './core/pages/strategy/strategy.page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'auth', component: AuthPageComponent },
+  { path: 'strategy', component: StrategyPageComponent },
   {
     path: 'battle',
     loadChildren: './features/battle/battle.module#BattleModule'
