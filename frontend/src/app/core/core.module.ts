6839aae6102da0bb3cf1e1663ad3f672624bda22
Gergo Gyori <gygergo93@gmail.com>
2020-10-02 09:31:40 +0200

    Implemented sidebar component

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
-import {RouterModule} from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import { AuthPageComponent } from './pages/auth/auth.page.component';
 import { StrategyPageComponent } from './pages/strategy/strategy.page.component';
 import { StatsComponent } from './components/stats/stats.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+
 
 @NgModule({
     declarations: [
         AuthPageComponent,
         StrategyPageComponent,
         StatsComponent,
-        SidebarComponent
+        SidebarComponent,
+        ProfileCardComponent
       ],
       imports: [
         BrowserModule,
