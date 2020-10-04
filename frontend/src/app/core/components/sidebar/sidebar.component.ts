a7507da63e5c1891275ca364a5a26b916921323c
Gergo Gyori <gygergo93@gmail.com>
2020-10-04 02:23:23 +0200

    Added routing and dialog logic to sidebar menu

--- frontend/src/app/core/components/sidebar/sidebar.component.ts
+++ frontend/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { BuildingsPageComponent } from 'src/app/features/buildings/pages/buildings/buildings.page.component';
+import { ComponentType } from '@angular/cdk/portal';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  logo = 'Undersea';
+  isDialogOpen = false;
+  components = BuildingsPageComponent;
+  features: object[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.features = [
+      { name: 'Épületek', route: 'features/buildings', component: BuildingsPageComponent },
+      { name: 'Támadás', route: 'features/buildings', component: BuildingsPageComponent },
+      { name: 'Fejlesztések', route: 'features/buildings', component: BuildingsPageComponent },
+      { name: 'Harc', route: 'features/buildings', component: BuildingsPageComponent },
+      { name: 'Ranglista', route: 'features/buildings', component: BuildingsPageComponent },
+      { name: 'Sereg', route: 'features/buildings', component: BuildingsPageComponent }
+    ];
+
+    if (!this.isDialogOpen) {
+      this.router.navigate(['strategy']);
+    }
+  }
+
+  openFeatureDialog(featureRoute: string, featureComp: ComponentType<BuildingsPageComponent>) {
+    this.isDialogOpen = true;
+    this.router.navigate([featureRoute], { relativeTo: this.route });
+    let dialogRef = this.dialog.open(featureComp);
+    dialogRef.afterClosed().subscribe(res => {
+      this.isDialogOpen = false;
+      this.router.navigate(['strategy']);
+    });
   }
 
 }
