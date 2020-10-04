206fc5ae585f448e5f0226ae823661de0d6f4bef
Gergo Gyori <gygergo93@gmail.com>
2020-10-04 02:10:10 +0200

    Added angular material, moved global scss

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {MatDialogModule} from '@angular/material';
 
 @NgModule({
+    declarations: [
+    ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule,
+        MatDialogModule,
     ],
     exports: [
         CommonModule
