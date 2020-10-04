36e1c82bd1fbb98f2963263488714824e7872928
Gergo Gyori <gygergo93@gmail.com>
2020-10-04 02:45:31 +0200

    Added auth guard (mock)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { AuthPageComponent } from './core/pages/auth/auth.page.component';
 import { StrategyPageComponent } from './core/pages/strategy/strategy.page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'auth', component: AuthPageComponent },
-  { path: 'strategy', component: StrategyPageComponent, children: [
+  { path: 'strategy', component: StrategyPageComponent, canActivate: [AuthGuard], children: [
     { path: 'features/buildings', loadChildren: './features/buildings/buildings.module#BuildingsModule' }
   ] },
   {
--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
-@Injectable()
-export class AuthGuard {
-    
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    isAuthenticated = true;
+
+    constructor(private router: Router) { }
+    canActivate(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+        if (this.isAuthenticated) {
+            return true;
+        } else {
+            this.router.navigate(['auth']);
+            return false;
+        }
+    }
 }
