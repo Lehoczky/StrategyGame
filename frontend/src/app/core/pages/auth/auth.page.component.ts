74a9b5002c799a3de6ab4916afafc1c58fb27e11
Lehoczky Zoltán <lehoczky.zoltan@encosoft-dev.hu>
2020-10-05 17:28:48 +0200

    login form uses the backend for authentication

--- frontend/src/app/core/pages/auth/auth.page.component.ts
+++ frontend/src/app/core/pages/auth/auth.page.component.ts
@@ -1,43 +1,76 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './auth.page.component.html',
-  styleUrls: ['./auth.page.component.scss']
+  styleUrls: ['./auth.page.component.scss'],
 })
 export class AuthPageComponent implements OnInit {
-
   @ViewChild('f', { static: false }) authForm: NgForm;
+
   title = 'Undersea';
   isSignUp = false;
   matchedPasswords = false;
+  returnUrl: string;
+  error: string;
 
-  constructor(private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthService,
+  ) {
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['strategy']);
+    }
+  }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || 'strategy';
   }
 
-  onAuth(isSignUp) {
+  get username(): string {
+    return this.authForm.value.username;
+  }
+
+  get password(): string {
+    return this.authForm.value.password;
+  }
+
+  get passwordAgain(): string {
+    return this.authForm.value.passwordAgain;
+  }
+
+  onAuth(isSignUp: boolean): void {
     isSignUp ? this.onSignUp() : this.onLogin();
-    this.router.navigate(['strategy']);
   }
 
-  onLogin() {
-    console.log('logging in...')
+  onLogin(): void {
+    this.authenticationService
+      .login(this.username, this.password)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        errorResponse => {
+          this.error = errorResponse.error.message;
+        },
+      );
   }
 
-  onSignUp() {
-    console.log('signing up...')
+  onSignUp(): void {
+    console.log('signing up...');
   }
 
-  checkPasswords() {
-    this.matchedPasswords = this.authForm.value.password === this.authForm.value.passwordAgain;
+  checkPasswords(): void {
+    this.matchedPasswords = this.password === this.passwordAgain;
   }
 
-  switchForm() {
+  switchForm(): void {
     this.isSignUp = !this.isSignUp;
     this.authForm.reset();
   }
-
 }
---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
+import { User } from '../models/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private currentUserSubject$: BehaviorSubject<User>;
+  currentUser$: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject$ = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser')),
+    );
+    this.currentUser$ = this.currentUserSubject$.asObservable();
+  }
+
+  /** Return the logged in user. */
+  public get currentUserValue(): User {
+    return this.currentUserSubject$.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post<User>(`${environment.apiUrl}/auth/login`, {
+        username,
+        password,
+      })
+      .pipe(
+        tap(user => {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject$.next(user);
+        }),
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject$.next(null);
+  }
+}
