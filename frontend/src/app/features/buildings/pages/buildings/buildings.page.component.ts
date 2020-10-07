3d4c12fa91482de6636f0251b5d2a488f9ddced6
Lehoczky Zoltán <lehoczky.zoltan@encosoft-dev.hu>
2020-10-07 13:30:56 +0200

    can purchase buildings

--- frontend/src/app/features/buildings/pages/buildings/buildings.page.component.ts
+++ frontend/src/app/features/buildings/pages/buildings/buildings.page.component.ts
@@ -1,32 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BuildingService } from 'src/app/core/services/building.service';
 
 @Component({
   templateUrl: './buildings.page.component.html',
-  styleUrls: ['./buildings.page.component.scss']
+  styleUrls: ['./buildings.page.component.scss'],
 })
-export class BuildingsPageComponent implements OnInit {
+export class BuildingsPageComponent {
   selectedIndex: number;
+  selectedBuilding: any;
+  buildings$ = this.buildingService.buildingsWithDescription$;
 
-  readonly buildingsInStore: any[] = [
-    {
-      name: 'Zátonyvár', price: 1000, units: 200, population: 0, coralPerTurn: 0,
-      img: '../../../../../../assets/img/undersea_game-07.png', owned: 0,
-      description: '200 egységnek nyújt szállást'
-    },
-    {
-      name: 'Áramlásirányító', price: 1000, units: 0, population: 50, coralPerTurn: 200,
-      img: '../../../../../../assets/img/undersea_game-05.png', owned: 0,
-      description: `50 embert ad a népességhez. 200 korallt termel körönként`
-    }
-  ];
+  readonly buildingsInStore: any[] = [];
 
-  constructor() { }
+  constructor(public buildingService: BuildingService) {}
 
-  ngOnInit() {
+  selectBuilding(index: number, building) {
+    this.selectedIndex = index;
+    this.selectedBuilding = building;
   }
 
-  selectBuilding(index: number) {
-    this.selectedIndex = index;
+  purchaseBuilding() {
+    this.buildingService.purchaseBuilding(this.selectedBuilding.typeName);
   }
 }
-
