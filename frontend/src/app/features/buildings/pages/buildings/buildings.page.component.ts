592ee62f562e98108c8de4954396d27a015b81d6
Gergo Gyori <gygergo93@gmail.com>
2020-10-07 12:36:05 +0200

    Refactored buildings feature template

--- frontend/src/app/features/buildings/pages/buildings/buildings.page.component.ts
+++ frontend/src/app/features/buildings/pages/buildings/buildings.page.component.ts
@@ -1,21 +1,32 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './buildings.page.component.html',
   styleUrls: ['./buildings.page.component.scss']
 })
 export class BuildingsPageComponent implements OnInit {
-  @ViewChild('buildingFirst', { static: false }) buildingFirst: ElementRef;
-  @ViewChild('buildingSecond', { static: false }) buildingSecond: ElementRef;
+  selectedIndex: number;
+
+  readonly buildingsInStore: any[] = [
+    {
+      name: 'Zátonyvár', price: 1000, units: 200, population: 0, coralPerTurn: 0,
+      img: '../../../../../../assets/img/undersea_game-07.png', owned: 0,
+      description: '200 egységnek nyújt szállást'
+    },
+    {
+      name: 'Áramlásirányító', price: 1000, units: 0, population: 50, coralPerTurn: 200,
+      img: '../../../../../../assets/img/undersea_game-05.png', owned: 0,
+      description: `50 embert ad a népességhez. 200 korallt termel körönként`
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  selectBuilding(building: number) {
-    (building === 1) ?
-      this.buildingFirst.nativeElement.tabIndex = 1 : this.buildingSecond.nativeElement.tabIndex = 1;
-    }
+  selectBuilding(index: number) {
+    this.selectedIndex = index;
   }
+}
 
