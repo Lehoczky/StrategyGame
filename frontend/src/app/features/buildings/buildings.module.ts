31965dc6ef49d71263f79ed28987f29c891efd9b
Lehoczky Zoltán <lehoczky.zoltan@encosoft-dev.hu>
2020-10-07 12:22:32 +0200

    Add purchase building funtionality to service

--- frontend/src/app/features/buildings/buildings.module.ts
+++ frontend/src/app/features/buildings/buildings.module.ts
@@ -2,18 +2,9 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BuildingsRoutingModule } from './buildings-routing.module';
 import { BuildingsPageComponent } from './pages/buildings/buildings.page.component';
-import { BuildingService } from './services/building.service';
 
 @NgModule({
-    imports: [
-        SharedModule,
-        BuildingsRoutingModule
-    ],
-    providers: [
-        BuildingService
-    ],
-    declarations: [
-        BuildingsPageComponent
-    ]
+  imports: [SharedModule, BuildingsRoutingModule],
+  declarations: [BuildingsPageComponent],
 })
-export class BuildingsModule { }
+export class BuildingsModule {}
